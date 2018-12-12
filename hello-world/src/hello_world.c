b01256416f01a4a81c7a9347dfa78cd1b80da71a
Joshua Ferrell <ferrell.joshua@gmail.com>
2018-12-12 13:17:08 -0500

    Hello World
    Hello World exercise on the C track on exercism.io

---
+++ hello-world/src/hello_world.c
@@ -0,0 +1,12 @@
+/*	Project: Exercism C - Hello World
+	Author: Joshua Ferrell
+	Filename: hello_world.c
+	Start Date: 12/12/2018
+	Last Update Date: 12/12/2018	*/
+
+#include "hello_world.h"
+
+const char *hello(void) {
+
+println("Hello World");
+}
